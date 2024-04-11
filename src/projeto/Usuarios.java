0ae0f33f83b407e333dabd68c852510a0eb21618
Levi <levilimabezerra20@gmail.com>
2024-04-11 15:07:34 -0300

    adicionando metodos

---
+++ src/projeto/Usuarios.java
@@ -0,0 +1,43 @@
+package projeto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuarios {
+	
+	private List<Usuario> usuarios;
+	
+	public Usuarios() {
+		this.usuarios = new ArrayList();
+	}
+	
+	public Usuario adicionarUsuarios(String nome, String matricula, String senha, String tipo) {
+		Usuario usuario = new Usuario(nome, matricula, senha, tipo);
+		usuarios.add(usuario);
+		System.out.println("Usuario cadastrado com sucesso!");
+		return usuario; 
+	}
+	
+	public int encontarUsuario(String matricula) {
+		for(int i = 0; i < usuarios.size(); i++) {
+			if(usuarios.get(i).getMatricula().equals(matricula)) {
+				return i;
+			}
+		}
+		return -1; 
+	}
+	
+	public void removerUsuario(String matricula) {
+		int indice = this.encontarUsuario(matricula);
+		if(indice != -1) {
+			usuarios.remove(indice);
+			System.out.println("Usuario removido com sucesso!");
+		} else {
+			System.out.println("Usuario nao encontrado!");
+		}
+	}
+	
+	public List<Usuario> getUsuarios() {
+		return this.usuarios; 
+	}
+}
